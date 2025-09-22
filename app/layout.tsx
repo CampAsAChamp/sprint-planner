a26691a8faa4aa943dbd4ac8741e59624f4944c4
Nick Schneider <nickschneider101@gmail.com>
2025-09-21 19:30:07 -0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="apple-mobile-web-app-title" content="Sprint Planner" />
       <body className={inter.className}>{children}</body>
     </html>
   )
